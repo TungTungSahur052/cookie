772f10e1a036ebeb5a8130266ddbaab0553db6a5
Mirra <chiuchiu@Mirras-Air.lan>
2026-08-11 20:09:06 +0700

    hellohello

---
+++ Bai6.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import time 
+st.title("Order food")
+food1 = st.text_input('What do you want to eat? ')
+drink1 = st.text_input('What do you want to drink?')
+
+if st.button("Confirm Order"):
+    st.write("Your oder: ")
+    st.write("Food: ",food1)
+    st.write("Drink: ",drink1)
+
+
+st.title("My progress bar: ")
+myBar = st.progress(0)
+
+for percentComplete in range(100):
+    time.sleep(0.05)
+    myBar.progress(percentComplete+1)
+
+st.balloons()
